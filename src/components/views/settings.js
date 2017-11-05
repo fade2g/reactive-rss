9d351beddd47fd8c9a07394c9e1ebface4393b27
fade2g <holger.heymanns@gmail.com>
2017-11-05 22:24:33 +0100

    feature: Add form controls (no business logic)

--- src/components/views/settings.js
+++ src/components/views/settings.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
+import {Button, Form} from "semantic-ui-react";
+
 
 class Settings extends Component {
 
   render() {
     return (
       <div>
-        <h1>Hello</h1>
-        <p>world</p>
+        <Form>
+          <Form.Field>
+            <label>Server URL</label>
+            <input placeholder="Server URL" required/>
+          </Form.Field>
+
+          <Form.Field>
+            <label>User name</label>
+            <input placeholder="User name" required/>
+          </Form.Field>
+
+          <Form.Field>
+            <label>Password</label>
+            <input type="password" placeholder="Password" required/>
+          </Form.Field>
+
+          <Button type='submit'>Submit</Button>
+        </Form>
       </div>
     )
   }

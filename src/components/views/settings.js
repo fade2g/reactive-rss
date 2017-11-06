7560527efebd808cc09b039c14360a5971c27f03
fade2g <holger.heymanns@gmail.com>
2017-11-06 11:51:21 +0100

    feature: Add state to form controls
    Buttons (Test connection, Save) not yet implemented
    
    Not stored in redux store

--- src/components/views/settings.js
+++ src/components/views/settings.js
@@ -4,30 +4,64 @@ import {Button, Form} from "semantic-ui-react";
 
 class Settings extends Component {
 
+  state = {
+    serverUrl: process.env.REACT_APP_DEFAULT_HOST_URL || '',
+    userName: process.env.REACT_APP_DEFAULT_USER_NAME || '',
+    password: ''
+  };
+
+  handleChange = (event) => {
+    console.log(event.target.name + ", " + event.target.value);
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+
+  };
+
   render() {
+    const {serverUrl, userName, password} = this.state;
+    const submitActive = serverUrl && userName && password;
     return (
       <div>
         <Form>
           <Form.Field>
             <label>Server URL</label>
-            <input placeholder="Server URL" required/>
+            <input type="text"
+                   placeholder="Server URL"
+                   name="serverUrl"
+                   value={serverUrl}
+                   onChange={this.handleChange}
+                   required/>
           </Form.Field>
 
           <Form.Field>
             <label>User name</label>
-            <input placeholder="User name" required/>
+            <input type="text"
+                   placeholder="User name"
+                   name="userName"
+                   value={userName}
+                   onChange={this.handleChange}
+                   required/>
           </Form.Field>
 
           <Form.Field>
             <label>Password</label>
-            <input type="password" placeholder="Password" required/>
+            <input type="password"
+                   placeholder="Password"
+                   name="password"
+                   value={password}
+                   onChange={this.handleChange}
+                   required/>
           </Form.Field>
-
-          <Button type='submit'>Submit</Button>
+          <Form.Group>
+            <Button disabled={!submitActive}>Test Connection</Button>
+            <Button type='submit' disabled={!submitActive}>Save</Button>
+          </Form.Group>
         </Form>
       </div>
     )
   }
 }
+
 
 export default Settings;
